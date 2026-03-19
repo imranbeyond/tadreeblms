4a4fc78d499628e60b933957d0ded19c743ee879
imranbeyond <imran.beyondtech@gmail.com>
2026-03-19 15:26:21 +0530

    fix ajax error

--- public/install_ajax.php
+++ public/install_ajax.php
@@ -333,6 +333,8 @@ try {
             $env = file_get_contents($envFile);
             if (strpos($env, 'APP_INSTALLED=') === false) {
                 $env .= "\nAPP_INSTALLED=true\n";
+            } else {
+                $env = preg_replace('/APP_INSTALLED=.*/', 'APP_INSTALLED=true', $env);
             }
 
             file_put_contents($envFile, $env);
